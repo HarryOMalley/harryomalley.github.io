6ef83ff4c6f585c6ba6c9491372e683a4822f257
Harry <harryomalley2@gmail.com>
2023-03-10 17:50:40 +0000

    Slight formatting change for main section

--- src/components/MainTitle.tsx
+++ src/components/MainTitle.tsx
@@ -2,12 +2,10 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const MainTitle = () => {
     return (
-        <div className="flex h-1/5 flex-col items-center justify-between text-center">
-            <div>
-                <h1 className="font-mono text-5xl font-bold">Harry O'Malley</h1>
-                <h2 className="mt-7 text-3xl">Engineer</h2>
-            </div>
-            <div className="flex w-full flex-row justify-evenly text-5xl">
+        <div className="flex h-1/6 flex-col items-center justify-between text-center">
+            <h1 className="font-mono text-5xl font-bold">Harry O'Malley</h1>
+            <h2 className="text-3xl">Engineer</h2>
+            <div className="flex w-1/2 flex-row justify-between text-5xl">
                 <a href="https://github.com/HarryOMalley" target="_blank">
                     <FaGithub />
                 </a>
