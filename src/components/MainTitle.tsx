19fe24a28d4c930b67ddc9656c3a4477ccd44bb1
Harry <harryomalley2@gmail.com>
2023-03-10 17:45:34 +0000

    Modified Styling slightly

--- src/components/MainTitle.tsx
+++ src/components/MainTitle.tsx
@@ -2,10 +2,12 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const MainTitle = () => {
     return (
-        <div className="text-center">
-            <h1 className="font-mono text-3xl font-bold">Harry O'Malley</h1>
-            <h2 className="mt-3">Engineer</h2>
-            <div className="mt-4 flex flex-row justify-evenly text-2xl">
+        <div className="flex h-1/5 flex-col items-center justify-between text-center">
+            <div>
+                <h1 className="font-mono text-5xl font-bold">Harry O'Malley</h1>
+                <h2 className="mt-7 text-3xl">Engineer</h2>
+            </div>
+            <div className="flex w-full flex-row justify-evenly text-5xl">
                 <a href="https://github.com/HarryOMalley" target="_blank">
                     <FaGithub />
                 </a>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (
-        <div className="flex h-8 w-screen items-center justify-end bg-black">
-            <div className="mr-3 flex w-1/3 flex-row justify-between text-xs">
+        <div className="flex h-12 w-screen items-center justify-end bg-black">
+            <div className="mr-3 flex w-1/3 flex-row justify-between">
                 <h1 className="p-1 text-gray-200">About</h1>
                 <h1 className="p-1 text-gray-200">Projects</h1>
                 <h1 className="p-1 text-gray-200">Contact</h1>
