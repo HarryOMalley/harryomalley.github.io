6bd2c29e117681556b3aa9c2521bd4d6e12e6865
Harry <harryomalley2@gmail.com>
2023-03-13 19:14:50 +0000

    Expanded tailwind config with extra large values

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            width: {
+                128: '32rem',
+                192: '48rem',
+                256: '64rem',
+                320: '80rem',
+            },
+        },
     },
     plugins: [],
 };
